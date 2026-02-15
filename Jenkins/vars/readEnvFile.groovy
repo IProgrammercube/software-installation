3b93c17126f46739388dd183d8943ca14cafc26f
Programmercube <160624779+IProgrammercube@users.noreply.github.com>
2026-02-15 17:01:34 +0530

    Update readEnvFile.groovy

--- Jenkins/vars/readEnvFile.groovy
+++ Jenkins/vars/readEnvFile.groovy
@@ -1,1 +1,14 @@
+def call(String filePath) {
+    def map = [:]
+
+    readFile(filePath).split("\n").each { line ->
+        line = line.trim()
+
+        if (line && !line.startsWith("#")) {
+            def (key, value) = line.split("=", 2)
+            map[key.trim()] = value.trim()
+        }
+    }
+    return map
+}
 
